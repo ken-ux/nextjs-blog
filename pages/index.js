b9cde42dc5fcadafe5313f028378b5c836074923
Kenny Nguyen <sokenns@gmail.com>
2021-12-22 17:03:31 -0800

    Change name

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Kenny and this is my test website using Next.js!</p>
+        <p>Hello, I'm Ken and this is my test website using Next.js!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
